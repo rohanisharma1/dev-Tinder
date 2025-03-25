5375ecd2cc87c07f6239df038fd38415d36eae33
Rohani Sharma <sharmarohani358@gmail.com>
2025-03-25 17:58:36 +0530

    multiple routers

--- src/app.js
+++ src/app.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const app = express();
-// this handle only get call to /user
-app.get("/user",(req, res)=>{
-    console.log("handles the request");
-   res.send({firstName:"Rohani",lastName :"Sharma"})
-  });
-  app.post("/user",(req, res)=>{
-    console.log(req.body);
-    // saving the data to database.
-    res.send("database test");
- });
+//app.use ("/route ,rh,rh2,rh3...")
+app.get(
+  "/user",
+  (req, res,next)=>{
+  console.log("handleing the route user!!");
+  next();
+     },
+     (req,res,next)=>{
+      console.log("handleing the route user2!!");
+      next();
 
-  app.delete("/user",(req,res) =>{
-    res.send("deleted a successfully");
-});
-//this will match all the  HTTP method API calls to test
+     },
+     (req,res,next)=>{
+      console.log("handleing the route user3!!");
+      next();
 
+     }, 
+     (req,res,next)=>{
+      console.log("handleing the route user4!!");
+      next();
 
+     },
+     (req,res,next)=>{
+      console.log("handleing the route user5!!");
+      res.send("5th respone");
 
+     },
+
+);
+
+ 
 app.listen(7777,() =>{
     console.log("server is successfully listening on port7777");
 
